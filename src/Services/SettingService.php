0d395dc6592d1ad0af6d187e82a523bae4fdbb31
Davut Kember <davutkmbr@gmail.com>
2023-01-06 17:24:17 +0300

    Remove type

--- src/Services/SettingService.php
+++ src/Services/SettingService.php
@@ -28,7 +28,7 @@ class SettingService
         return $default;
     }
 
-    public function getModel(string $key, ?Model $model = false): ?Setting
+    public function getModel(string $key, $model = false): ?Setting
     {
         if ($model === false) {
             return Setting::query()
